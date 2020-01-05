7bddd08631c1011f1b9dc85ed05c67dcac1000d8
zcribe <erlend.eelmets@gmail.com>
2020-01-05 10:19:05 +0200

    Test init

--- core/views.py
+++ core/views.py
@@ -4,8 +4,9 @@ from django.views.generic import ListView, DetailView, CreateView, DeleteView, U
 from django.contrib.auth.decorators import login_required
 from django.utils.decorators import method_decorator
 from django.contrib.postgres.search import SearchQuery, SearchVector, SearchRank
+from django.db.models import Prefetch
 
-from .models import Category, Advertisement
+from .models import Category, Advertisement, AdvertisementImage
 
 
 class IndexView(ListView):
@@ -15,7 +16,7 @@ class IndexView(ListView):
     def get_context_data(self, **kwargs):
         context = super(IndexView, self).get_context_data(**kwargs)
         context['annotated_list'] = Category.get_annotated_list()
-        context['ads'] = Advertisement.public.get_queryset()[:30]
+        context['ads'] = Advertisement.public.prefetch_related('advertisement')[:30]
         return context
 
 
