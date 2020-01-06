1004fe8a27f3bc75510652b63928fa377030b58f
zcribe <erlend.eelmets@gmail.com>
2020-01-06 12:00:08 +0200

    Theme swap

--- config/settings/base.py
+++ config/settings/base.py
@@ -110,9 +110,12 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = 'static'
 MEDIA_URL = '/media/'
 MEDIA_ROOT = 'media'
+
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, "static"),
+]
 
 # AUTHENTICATION
 AUTHENTICATION_BACKENDS = (
