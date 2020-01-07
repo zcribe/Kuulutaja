58b014f50d489ee8fda638f66db881090912a593
zcribe <erlend.eelmets@gmail.com>
2020-01-07 16:39:31 +0200

    Profile subpages initial

--- users/urls.py
+++ users/urls.py
@@ -3,4 +3,8 @@ from .views import *
 
 urlpatterns = [
     path('accounts/profile', ProfileView.as_view(), name='profile'),
+    path('accounts/profile/email', ProfileEmailView.as_view(), name='profile-email'),
+    path('accounts/profile/password', ProfilePasswordView.as_view(), name='profile-password'),
+    path('accounts/profile/settings', ProfileSettingsView.as_view(), name='profile-settings'),
+    path('accounts/profile/privacy', ProfilePrivacyView.as_view(), name='profile-privacy'),
 ]
--- users/views.py
+++ users/views.py
@@ -3,6 +3,7 @@ from django.views.generic import DetailView, TemplateView
 from django.contrib.auth.decorators import login_required
 from django.utils.decorators import method_decorator
 
+from core.models import Advertisement
 from .models import Profile
 
 
@@ -13,6 +14,25 @@ class ProfileView(TemplateView):
     def get_context_data(self, **kwargs):
         context = super(ProfileView, self).get_context_data(**kwargs)
         context['profile'] = Profile.objects.filter(user__username=self.request.user.get_username())
+        context['my_adverts'] = Advertisement.objects.filter(owner=self.request.user.pk)
         return context
 
 
+@method_decorator(login_required, name='dispatch')
+class ProfileEmailView(TemplateView):
+    template_name = 'users/profile_email.html'
+
+
+@method_decorator(login_required, name='dispatch')
+class ProfilePasswordView(TemplateView):
+    template_name = 'users/profile_password.html'
+
+
+@method_decorator(login_required, name='dispatch')
+class ProfileSettingsView(TemplateView):
+    template_name = 'users/profile_settings.html'
+
+
+@method_decorator(login_required, name='dispatch')
+class ProfilePrivacyView(TemplateView):
+    template_name = 'users/profile_privacy.html'
