0c7bb1d9250106b0fe5bfc29cb41ccb685f3afa5
zcribe <erlend.eelmets@gmail.com>
2020-01-08 16:50:27 +0200

    Bootstrapified messages

--- config/settings/base.py
+++ config/settings/base.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/3.0/ref/settings/
 
 import os
 import environ
+
 from celery.schedules import crontab
 
 env = environ.Env()
@@ -156,3 +157,12 @@ CELERY_RESULT_SERIALIZER = 'json'
 
 
 CRISPY_TEMPLATE_PACK = 'bootstrap4'
+
+from django.contrib.messages import constants as messages
+MESSAGE_TAGS = {
+    messages.DEBUG: 'alert-info',
+    messages.INFO: 'alert-info',
+    messages.SUCCESS: 'alert-success',
+    messages.WARNING: 'alert-warning',
+    messages.ERROR: 'alert-danger',
+}
